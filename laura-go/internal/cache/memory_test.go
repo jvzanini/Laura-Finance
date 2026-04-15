01494b691dc7065f3bd36e6a13fd2de574dc9e87
jvzanini <zanini107@gmail.com>
2026-04-15 07:32:18 -0300

    test(cache): cobre InMemoryCache TTL/LRU/Invalidate

---
+++ laura-go/internal/cache/memory_test.go
@@ -0,0 +1,47 @@
+package cache
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestInMemoryCache_TTLExpiry(t *testing.T) {
+	c, _ := NewInMemoryCache(10)
+	ctx := context.Background()
+	c.Set(ctx, "k", []byte("v"), 50*time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
+	_, hit, _ := c.Get(ctx, "k")
+	if hit {
+		t.Error("expected miss after TTL")
+	}
+}
+
+func TestInMemoryCache_LRUEviction(t *testing.T) {
+	c, _ := NewInMemoryCache(2)
+	ctx := context.Background()
+	c.Set(ctx, "a", []byte("1"), time.Minute)
+	c.Set(ctx, "b", []byte("2"), time.Minute)
+	c.Set(ctx, "c", []byte("3"), time.Minute)
+	_, hit, _ := c.Get(ctx, "a")
+	if hit {
+		t.Error("expected 'a' evicted")
+	}
+}
+
+func TestInMemoryCache_InvalidatePattern(t *testing.T) {
+	c, _ := NewInMemoryCache(10)
+	ctx := context.Background()
+	c.Set(ctx, "ws:123:foo", []byte("1"), time.Minute)
+	c.Set(ctx, "ws:123:bar", []byte("2"), time.Minute)
+	c.Set(ctx, "ws:456:foo", []byte("3"), time.Minute)
+	c.Invalidate(ctx, "ws:123:*")
+	_, hit, _ := c.Get(ctx, "ws:123:foo")
+	if hit {
+		t.Error("ws:123:foo should be invalidated")
+	}
+	_, hit, _ = c.Get(ctx, "ws:456:foo")
+	if !hit {
+		t.Error("ws:456:foo should NOT be invalidated")
+	}
+}
